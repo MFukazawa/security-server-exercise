6d28b5dda5abc81fba3ae83a0daa10e9ea813d9a
MFukazawa <micahfukazawa@gmail.com>
2021-06-09 12:52:41 +0900

    Add morgan + winston

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
-const winston = require('winston');
+const winston = require('winston')
+// const morgan = require('morgan')
 
 const bodyParser = require('body-parser');
 const app = express()
 app.use(cors())
 app.use(helmet())
 app.use(bodyParser.json())
+// app.use(morgan('tiny'))
+// POST /secret 200 9 - 2.548 ms
+
+// app.use(morgan('combined'))
+// ::1 - - [09/Jun/2021:03:41:18 +0000] "POST /secret HTTP/1.1" 200 9
+// "http://127.0.0.1:5500/" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)
+// AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36"
+
+const files = new winston.transports.File({ filename: 'combined.log' });
+winston.add(files);
+
+// const logger = winston.createLogger({
+//   level: 'info',
+//   format: winston.format.json(),
+//   defaultMeta: { service: 'user-service' },
+//   transports: [
+//     new winston.transports.File({ filename: 'error.log', level: 'error' }),
+//     new winston.transports.File({ filename: 'combined.log '})
+//   ]
+// })
+
+// logger.add(new winston.transports.Console({
+//   format: winston.format.simple()
+// }))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
